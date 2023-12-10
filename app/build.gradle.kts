8959e80266f74a961a6d1e737f57f7f6230bd2be
Przemysław Kłos <pklos1992@gmail.com>
2023-12-10 15:30:48 +0100

    CameraX gradle implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,21 +50,37 @@ android {
         }
     }
 }
+val compose_ui_version = "1.8.1"
+val camerax_version = "1.4.0-alpha02"
+val compose_bom_version = "2023.03.00"
+val junit_version = "4.13.2"
+val junit_test_ext_version = "1.1.5"
+val espresso_core_version = "3.5.1"
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    implementation("androidx.activity:activity-compose:$compose_ui_version")
+    implementation(platform("androidx.compose:compose-bom:$compose_bom_version"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
+
+    testImplementation("junit:junit:$junit_version")
+    androidTestImplementation("androidx.test.ext:junit:$junit_test_ext_version")
+    androidTestImplementation("androidx.test.espresso:espresso-core:")
+    androidTestImplementation(platform("androidx.compose:compose-bom:$compose_bom_version"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
