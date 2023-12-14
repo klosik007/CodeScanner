e80f093ab6a6248aadc914cf5da00081bd617878
Przemysław Kłos <pklos1992@gmail.com>
2023-12-14 13:43:39 +0100

    Camera preview in Compose view with permissions handler included

--- app/src/main/java/com/pklos/codescanner/ui/dashboard/HomeView.kt
+++ app/src/main/java/com/pklos/codescanner/ui/dashboard/HomeView.kt
@@ -1,9 +1,87 @@
 package com.pklos.codescanner.ui.dashboard
 
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.LinearLayout
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import android.Manifest
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.ExperimentalComposeUiApi
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 
 @Composable
 fun Home() {
+    CameraPermission()
+}
 
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPermission() {
+    val cameraPermissionsState = rememberPermissionState(Manifest.permission.CAMERA)
+    LaunchedEffect(key1 = Unit) {
+        if (!cameraPermissionsState.status.isGranted && !cameraPermissionsState.status.shouldShowRationale) {
+            cameraPermissionsState.launchPermissionRequest()
+        }
+    }
+
+    if (cameraPermissionsState.status.isGranted) {
+        CameraPreviewScreen()
+    } else {
+        NoCameraPermissionScreen(cameraPermissionState = cameraPermissionsState)
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun NoCameraPermissionScreen(cameraPermissionState: PermissionState) {
+    //TODO: view
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun CameraPreviewScreen() {
+    val ctx = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val cameraController = remember { LifecycleCameraController(ctx) }
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { padding: PaddingValues ->
+        AndroidView(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            factory = { ctx ->
+                PreviewView(ctx).apply {
+                    setBackgroundColor(Color.White.toArgb())
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                    scaleType = PreviewView.ScaleType.FILL_START
+                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                }.also { previewView ->
+                    previewView.controller = cameraController
+                    cameraController.bindToLifecycle(lifecycleOwner)
+                }
+            },
+            onReset = {},
+            onRelease = {
+                cameraController.unbind()
+            }
+        )
+    }
 }
 
