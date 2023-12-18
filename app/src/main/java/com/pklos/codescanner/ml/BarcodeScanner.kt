366927c1583028088ea40be534d4ecd44523d7e1
Przemysław Kłos <pklos1992@gmail.com>
2023-12-18 12:15:22 +0100

    Narrowing barcode formats (for example no QR codes detecting)

--- app/src/main/java/com/pklos/codescanner/ml/BarcodeScanner.kt
+++ app/src/main/java/com/pklos/codescanner/ml/BarcodeScanner.kt
@@ -32,6 +32,10 @@ class BarcodeScanner(val callback: () -> Unit): ImageAnalysis.Analyzer {
     }
 
     private val options = BarcodeScannerOptions.Builder()
-        .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+        .setBarcodeFormats(
+            Barcode.FORMAT_AZTEC, Barcode.FORMAT_CODABAR, Barcode.FORMAT_CODE_128,
+            Barcode.FORMAT_CODE_39, Barcode.FORMAT_CODE_93, Barcode.FORMAT_DATA_MATRIX,
+            Barcode.FORMAT_EAN_13, Barcode.FORMAT_EAN_8, Barcode.FORMAT_ITF, Barcode.FORMAT_PDF417,
+            Barcode.FORMAT_UPC_A, Barcode.FORMAT_UPC_E)
         .build()
 }
